7bdc0514cafca9b41319854bc0069519eec31fc6
SoKH-creator <skh7235@naver.com>
2025-07-14 19:42:28 +0900

    Setting

---
+++ 15TeamProject/Player.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Player
+{
+    public static int level;
+    public static string name;
+    public static string job;
+    public static int atk;
+    public static int def;
+    public static int hp;
+    public static int gold;
+
+
+    
+    // 싱글톤
+    private static Player? instance;
+    public static Player Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new Player();
+            }
+            return instance;
+        }
+    }
+
+
+        
+}
