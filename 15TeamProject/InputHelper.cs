62863b38890e5f26547a5b74e390a4781e96c616
SoKH-creator <skh7235@naver.com>
2025-07-15 16:02:54 +0900

    [feature] Input 처리할 스크립트 생성, 정수 입력 메서드 GetInt() 생성

---
+++ 15TeamProject/InputHelper.cs
@@ -0,0 +1,22 @@
+﻿internal static class Input
+{
+    // 사용 예시) int choice = Input.GetInt(); 와 같이 사용
+    public static int GetInt()
+    {
+       int num;
+
+        while (true)
+        {
+            string? input = Console.ReadLine();
+
+            if (int.TryParse(input, out num))
+            {
+                return num;
+            }
+            else
+            {
+                Console.WriteLine("잘못된 입력입니다.");
+            }
+        }
+    }    
+}
