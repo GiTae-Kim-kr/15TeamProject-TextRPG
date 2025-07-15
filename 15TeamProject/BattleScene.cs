a8d21d7a236aad2ae4d101976d61d9012f1d083f
GiTae Kim <132313723+GiTae-Kim-kr@users.noreply.github.com>
2025-07-15 14:55:45 +0900

    각 씬 연결완료 및 전투시작화면 추가
    공격 대기 화면에서 공격 선택하면 전투시작으로 넘어가고 -> PlayesrPhase로도 이동되게 씬 연결 및 생성 완료하였습니다.

--- 15TeamProject/BattleScene.cs
+++ 15TeamProject/BattleScene.cs
@@ -4,11 +4,13 @@ using System.Diagnostics;
 class BattleScene
 {
     Player player = Player.Instance;
-    private Monster[] monsterInfo;
+    public Monster[]? monsterInfo;       
 
     public void Run()
     {
-        Console.WriteLine("Battle!!\n\n");  // 한 칸 아래로 띄움
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("Battle!!\n");
+        Console.ResetColor();
         RandomMonster();// 몬스터 랜덤으로 출력하는 코드
         Console.WriteLine("\n\n[내정보]");
         Console.WriteLine($"Lv.{player.level}  Chad ({player.job})");
@@ -17,9 +19,36 @@ class BattleScene
         Console.Write("원하시는 행동을 입력해주세요. \n>>");
         string input = Console.ReadLine();  // 일단 아무거나 입력하면 PlayerPhase로 넘어감
 
-        // 임시
-        int target = 1; 
-        PlayerPhase(target);
+        if (input == "1") BattlePhase();  // 공격을 선택하면 BattlePhase로 넘어감
+        else
+        {
+            Console.WriteLine("잘못된 입력입니다. 다시 시도해주세요.");
+            Run();  // 1말고 다른거 입력하면 다시 Run() 메서드 호출 => 나중에 시작 화면으로 변경.
+        }
+
+
+    }
+
+    void BattlePhase()
+    {
+        int count = 1;
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("Battle!!\n");
+        Console.ResetColor();
+        foreach (Monster monster in monsterInfo)        // 랜덤으로 출력한 몬스터 정보 가져와서 다시 출력하기
+        {
+            Console.WriteLine($"{count} Lv.{monster.data.level} {monster.data.name}  HP {monster.hp}");
+            count++;
+        }
+        Console.WriteLine("\n\n[내정보]");
+        Console.WriteLine($"Lv.{player.level}  Chad ({player.job})");
+        Console.WriteLine($"HP : {player.hp}/100\n");    // 한 칸 띄움
+        Console.WriteLine("0. 취소\n");
+        Console.Write("대상을 선택해주세요. \n>>");
+        int target = int.Parse(Console.ReadLine());   // 몇 번 몬스터 맞출지.
+        if (target == 0) Run();
+        else PlayerPhase(target - 1);  // 0을 입력하면 취소로 작동되야해서 -1 해줌. 그 외의 숫자를 입력하면 PlayerPhase로 넘어감
     }
 
 
@@ -93,7 +122,7 @@ class BattleScene
 
         int monsterListIndex = MonsterDB.monsterList.Count; // 몬스터 리스트에 있는 몬스터 개수
         int monsterNumber = random.Next(1, 5); // 1부터 4까지의 랜덤 숫자 생성
-        Monster[] monsterInfo = new Monster[monsterNumber];    // 랜덤으로 뽑힌 몬스터 정보 저장 배열
+        monsterInfo = new Monster[monsterNumber];    // 랜덤으로 뽑힌 몬스터 정보 저장 배열
         for (int i = 0; i < monsterNumber; i++)
         {
             int monsterIndex = random.Next(0,monsterListIndex); // 0부터 2까지의 랜덤 숫자 생성 -> 하드코딩했던거 수정했습니다.
