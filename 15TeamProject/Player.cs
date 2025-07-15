98dcf04036e750fc11891154726ef0e5a0dc7c36
SoKH-creator <skh7235@naver.com>
2025-07-15 10:29:54 +0900

    [feature] Attack 메서드를 Player 클래스에 추가

--- 15TeamProject/Player.cs
+++ 15TeamProject/Player.cs
@@ -9,6 +9,7 @@ class Player
     public int hp;
     public int gold;
 
+    private Random rand = new Random();
 
     
     // 싱글톤
@@ -26,5 +27,21 @@ class Player
     }
 
 
-        
+    private Player()
+    {
+        level = 1;
+        name = "르탄이";
+        job = "전사";
+        atk = 10;
+        def = 5;
+        hp = 100; // 초기 체력
+        gold = 1500; // 초기 골드
+    }
+
+    public int Attack()
+    {
+        int variance = (int)Math.Ceiling(atk * 0.1f); // 분산
+        int demage = rand.Next(atk - variance, atk + variance + 1);
+        return demage;
+    }
 }
