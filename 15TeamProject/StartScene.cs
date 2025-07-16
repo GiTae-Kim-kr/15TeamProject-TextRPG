a6d8ba55a44a97471b5db05b325cf07953747ee9
GiTae Kim <132313723+GiTae-Kim-kr@users.noreply.github.com>
2025-07-16 10:56:03 +0900

    사소한 에러 수정 ;

--- 15TeamProject/StartScene.cs
+++ 15TeamProject/StartScene.cs
@@ -20,7 +20,7 @@ namespace _15TeamProject
             Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
             Console.WriteLine("이제 전투를 시작할 수 있습니다.\n");
 
-            Console.WriteLine("1. 상태보기")
+            Console.WriteLine("1. 상태보기");
             Console.WriteLine("2. 인벤토리");
             Console.WriteLine("3. 전투 시작");
             Console.WriteLine("4. 회복 아이템");
