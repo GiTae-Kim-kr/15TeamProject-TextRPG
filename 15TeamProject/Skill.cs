8ff07f3a4180ebd5669baf0fb80781463535f25b
SoKH-creator <skh7235@naver.com>
2025-07-16 11:33:28 +0900

    [feature] [refactor] 스킬 생성자 추가, 스킬 DB 추가
    나중에 몬스터DB 처럼 사용 가능하게 구현할 예정입니다.

--- 15TeamProject/Skill.cs
+++ 15TeamProject/Skill.cs
@@ -10,6 +10,13 @@ internal abstract class Skill
     public int consumeMp;  // 소비마나
     public string description; // 설명 
 
+    public Skill(string name, int consumeMp, string description)
+    {
+        this.name = name;
+        this.consumeMp = consumeMp;
+        this.description = description;
+    }
+
     public void Description()  // 스킬 설명
     {
         Console.WriteLine( $"{name} - MP {consumeMp}" );
@@ -19,10 +26,21 @@ internal abstract class Skill
     public abstract void Use();  // 스킬 사용 - 각 클래스에서 구현
 }
 
+class SkillDB
+{
+    List<Skill> skillDB = new List<Skill>()
+    {
+        new AlphaStrike("", 1, "")
+    };
+}
+
 class AlphaStrike : Skill
 {
+    // 생성자
+    public AlphaStrike(string name, int consumeMp, string description) : base(name, consumeMp, description) {    }
+
     public override void Use()
     {
-        throw new NotImplementedException();
+        
     }
 }
