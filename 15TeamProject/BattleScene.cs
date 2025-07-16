e4233d396844da51c58f1aadac8e0072bf7e64f8
SoKH-creator <skh7235@naver.com>
2025-07-16 20:29:09 +0900

    BattleScene 스킬 관련 함수 분리

--- 15TeamProject/BattleScene.cs
+++ 15TeamProject/BattleScene.cs
@@ -313,46 +313,6 @@ partial class BattleScene
         }
 
     }
-
-    void SkillSelect() 
-    {
-        // 상단부는 이전 화면과 동일하게 출력
-        Console.Clear();
-        Console.ForegroundColor = ConsoleColor.Magenta;
-        Console.WriteLine("Battle!!\n");
-        Console.ResetColor();
-        foreach (Monster monster in monsterInfo)
-        {
-            string afterHp;
-            if (monster.isDead)
-            {
-                afterHp = "Dead";
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-            }
-            else
-            {
-                afterHp = monster.hp.ToString();
-            }
-
-            Console.WriteLine($"Lv.{monster.data.level} {monster.data.name}  HP {afterHp}");
-            Console.ResetColor();
-        }
-        Console.WriteLine("\n\n[내정보]");
-        Console.WriteLine($"Lv.{player.level}  {player.name} ({player.job})");
-        Console.WriteLine($"HP : {player.hp}/100\n");
-
-        // 스킬 목록 출력
-        int num = 0;
-        foreach (Skill skill in SkillDB.skillDB)
-        {
-            num++;
-            Console.WriteLine($"{num}. ");
-        }
-    }
-
-    void PlayerSkillPhase(Skill skill, List<Monster> monsters)
-    {
-        
-    }
-        
+     
+       
 }
