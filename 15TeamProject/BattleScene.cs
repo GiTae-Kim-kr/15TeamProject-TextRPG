b4dd4c9228d73fd6440110907d3295e88a36a884
SoKH-creator <skh7235@naver.com>
2025-07-16 20:24:22 +0900

    BattleScene class 분리

--- 15TeamProject/BattleScene.cs
+++ 15TeamProject/BattleScene.cs
@@ -3,7 +3,7 @@ using _15TeamProject;
 using System;
 using System.Diagnostics;
 
-class BattleScene
+partial class BattleScene
 {
     Player player = Player.Instance;
     private Monster[]? monsterInfo;       
@@ -354,27 +354,5 @@ class BattleScene
     {
         
     }
-
-    public void DroppedPotion()
-    {
-        Random random = new Random();
-        int dromHPPotion = random.Next(0, 100);
-        int dromMPPotion = random.Next(0, 100);
-        if (dromHPPotion < 30)
-        {
-            droppedHPPotion++;
-        }
-        if (dromMPPotion < 20)
-        {
-            droppedMPPotion++;
-        }
-    }
-
-    public void GetPotion()
-    {
-        player.potionCount += droppedHPPotion;
-        Console.WriteLine($"포션을 {droppedHPPotion} 개 획득 하셨습니다.");
-        droppedHPPotion = 0;
-    }
-
+        
 }
---
+++ 15TeamProject/BattleScene_DiopItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+partial class BattleScene
+{
+    public void DroppedPotion()
+    {
+        Random random = new Random();
+        int dromHPPotion = random.Next(0, 100);
+        int dromMPPotion = random.Next(0, 100);
+        if (dromHPPotion < 30)
+        {
+            droppedHPPotion++;
+        }
+        if (dromMPPotion < 20)
+        {
+            droppedMPPotion++;
+        }
+    }
+
+    public void GetPotion()
+    {
+        player.potionCount += droppedHPPotion;
+        Console.WriteLine($"포션을 {droppedHPPotion} 개 획득 하셨습니다.");
+        droppedHPPotion = 0;
+    }
+}
+
