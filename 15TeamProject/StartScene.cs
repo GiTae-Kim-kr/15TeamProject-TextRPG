394369984544b200f3c03371372f60fa12bd524d
GiTae Kim <132313723+GiTae-Kim-kr@users.noreply.github.com>
2025-07-16 10:36:04 +0900

    퀘스트 리스트, 퀘스트 씬 생성
    퀘스트를 리스트로 관리하기 위해 퀘스트 클래스를 생성.
    그리고 퀘스트 화면을 보기위한 씬 클래스를 생성.

--- 15TeamProject/StartScene.cs
+++ 15TeamProject/StartScene.cs
@@ -14,6 +14,8 @@ namespace _15TeamProject
         {
             BattleScene battleScene = new BattleScene();
             HpPotion hpPotion = new HpPotion();
+            QuestList questList= new QuestList();
+
             Console.Clear();
             Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
             Console.WriteLine("이제 전투를 시작할 수 있습니다.\n");
@@ -21,6 +23,7 @@ namespace _15TeamProject
             Console.WriteLine("1. 상태보기");
             Console.WriteLine("2. 전투 시작");
             Console.WriteLine("3. 회복 아이템");
+            Console.WriteLine("4. 퀘스트 목록");
             Console.WriteLine();
             Console.Write("원하시는 행동을 입력해주세요. \n>>");
             int choice = Input.GetInt();
@@ -40,6 +43,10 @@ namespace _15TeamProject
                     Console.Clear();
                     // 회복 아이템 사용 화면으로 이동
                     hpPotion.ViewPotionInfo();
+                    break;
+                case 4:
+                    Console.Clear();
+                    questList.QuestScene();
                     break;
                 default:
                     Console.WriteLine("잘못된 입력입니다. 다시 시도해주세요.");
