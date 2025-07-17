852c3af58811c482432b66d90ea3a4630414fb9a
SoKH-creator <skh7235@naver.com>
2025-07-17 10:52:37 +0900

    배틀씬에 스킬 타겟팅까지 작업
    이후 타겟팅한 몬스터에 공격 -> 몬스터 턴 진행으로 넘기기 작업중

--- 15TeamProject/BattleScene_SkillMethod.cs
+++ 15TeamProject/BattleScene_SkillMethod.cs
@@ -35,17 +35,81 @@ partial class BattleScene
         Console.WriteLine($"HP : {player.hp}/100\n");
 
         // 스킬 목록 출력
-        int num = 0;
+        int i = 0;
         foreach (Skill skill in SkillDB.skillDB)
         {
-            num++;
-            Console.WriteLine($"{num}. ");
+            i++;
+            Console.Write($"{i}. ");
+            skill.Describe();
+        }
+
+        // 입력 안내 출력
+        Console.WriteLine("0. 취소\n");
+        Console.WriteLine("원하시는 행동을 입력해주세요.");
+        Console.Write(">>");
+
+        // 입력 구분
+        int input = Input.GetInt(0, SkillDB.skillDB.Count);
+        if (input == 0)
+        {            
+            Run();
+            return;            
+        }
+        else
+        {
+            Skill skill = SkillDB.skillDB[input - 1];
+            List<Monster> monsters = new List<Monster>(monsterInfo);
+            PlayerSkillPhase(skill, monsters);
+            return;
         }
     }
 
     void PlayerSkillPhase(Skill skill, List<Monster> monsters)
     {
+        // 상단부는 이전 화면과 동일하게 출력
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("Battle!!\n");
+        Console.ResetColor();
+        foreach (Monster monster in monsterInfo)
+        {
+            string afterHp;
+            if (monster.isDead)
+            {
+                afterHp = "Dead";
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+            }
+            else
+            {
+                afterHp = monster.hp.ToString();
+            }
 
+            Console.WriteLine($"Lv.{monster.data.level} {monster.data.name}  HP {afterHp}");
+            Console.ResetColor();
+        }
+
+        // 선택된 스킬 설명
+        skill.Describe();
+        Console.WriteLine();
+
+        // 타겟팅
+        if (skill.IsAutoTargeting)  // 자동 대상 지정이면
+        {
+            Console.WriteLine("자동으로 대상을 지정합니다.");
+            List<Monster> aliveMonsters = new List<Monster>(monsterInfo);
+            foreach (Monster monster in monsterInfo)
+            {
+                if (!monster.isDead)
+                    aliveMonsters.Add(monster);
+            }
+            List<Monster> targetMonsters = skill.Targeting(aliveMonsters);
+        }
+        else
+        {
+            Console.WriteLine($"대상을 선택해주세요. (1 ~ {monsters.Count})\n>>");
+            List<Monster> targetMonsters = skill.Targeting(monsters);
+        }
+
+        // 작성 중... 타겟팅한 몬스터에 공격 -> 몬스터 턴 진행으로 넘김
     }
-
 }
--- 15TeamProject/Skill.cs
+++ 15TeamProject/Skill.cs
@@ -9,18 +9,20 @@ internal abstract class Skill
     protected string name;    // 스킬이름
     protected int consumeMp;  // 소비마나
     protected string description; // 설명 
-
+    protected bool isAutoTargeting; // 자동 타겟팅 여부
 
     // 읽기 전용
     public string Name => name;    
     public int ConsumeMp => consumeMp;  
     public string Description => description;
+    public bool IsAutoTargeting => isAutoTargeting;
 
-    public Skill(string name, int consumeMp, string description)
+    public Skill(string name, int consumeMp, string description, bool isAutoTargeting)
     {
         this.name = name;
         this.consumeMp = consumeMp;
         this.description = description;
+        this.isAutoTargeting = isAutoTargeting;
     }
 
     public void Describe()  // 스킬 설명
@@ -38,8 +40,8 @@ internal static class SkillDB
 {
     public static List<Skill> skillDB = new List<Skill>()
     {
-        new AlphaStrike("알파 스트라이크", 10, "공격력 * 2 로 하나의 적을 공격합니다."),
-        new DoubleStrike("더블 스트라이크", 15, "공격력 * 1.5 로 2명의 적을 랜덤으로 공격합니다.")
+        new AlphaStrike("알파 스트라이크", 10, "공격력 * 2 로 하나의 적을 공격합니다.", false),
+        new DoubleStrike("더블 스트라이크", 15, "공격력 * 1.5 로 2명의 적을 랜덤으로 공격합니다.", true),
 
     };
 }
@@ -47,24 +49,17 @@ internal static class SkillDB
 class AlphaStrike : Skill
 {
     // 생성자
-    public AlphaStrike(string name, int consumeMp, string description) : base(name, consumeMp, description) { }
+    public AlphaStrike(string name, int consumeMp, string description, bool isAutoTargeting) 
+        : base(name, consumeMp, description, isAutoTargeting) { }
 
+    // 대상 지정 메서드
     public override List<Monster> Targeting(List<Monster> monsters)
     {
         while (true)
         {
-            Console.WriteLine("타겟을 선택하세요. (1 ~ {0})", monsters.Count);
-            int target = Input.GetInt();
+           int target = Input.GetInt(1, monsters.Count);
 
-            // 유효 범위 체크
-            if (target >= 1 && target < monsters.Count)
-            {
-                return new List<Monster> { monsters[target - 1] };
-            }
-            else
-            {
-                Console.WriteLine("잘못된 번호입니다. 다시 입력해주세요.");
-            }
+           return new List<Monster> { monsters[target - 1] };
         }
     }
 
@@ -77,15 +72,17 @@ class AlphaStrike : Skill
 class DoubleStrike : Skill
 {
     // 생성자
-    public DoubleStrike(string name, int consumeMp, string description) : base(name, consumeMp, description) { }
+    public DoubleStrike(string name, int consumeMp, string description, bool isAutoTargeting) 
+        : base(name, consumeMp, description, isAutoTargeting) { }
 
     Random rand = new Random();
 
+    // 대상 지정 메서드
     public override List<Monster> Targeting(List<Monster> monsters)
     {
         // 대상이 1마리 일 경우
         if (monsters.Count < 2)
-        {
+        {            
             return new List<Monster>(monsters);
         }
 
@@ -93,6 +90,7 @@ class DoubleStrike : Skill
         Monster first = tempMonsters[rand.Next(0, tempMonsters.Count)];
         tempMonsters.Remove(first);
         Monster second = tempMonsters[rand.Next(0, tempMonsters.Count)];
+
         return new List<Monster> { first, second };
     }
 
